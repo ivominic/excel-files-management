aed19160ae96a67914b9a5daeb9abb94c495ef4a
Ivo Minic <ivo.minic@gmail.com>
2023-01-14 17:48:38 +0100

    Added delete method

--- index.js
+++ index.js
@@ -3,16 +3,28 @@ const readXlsxFile = require("read-excel-file/node");
 const xlsx = require("xlsx");
 
 const rootFolder = "/home/ivo/Desktop/nffis-excel/";
-let currentFile = "Indikacija.xlsx";
+let currentFile = "Osn_Krive_ZD.xlsx";
+//Krug.xlsx - ovo ponovo pustiti
 let currentTitle = "";
 let promiseArray = [];
 let dataArray = [];
 let isThereDifferentColumns = false;
-let completedFilesArray = ["GranicneLinije.xlsx"];
+let differenceCount = 0;
+let completedFilesArray = [
+  "GranicneLinije.xlsx",
+  "Indikacija.xlsx",
+  "KrgNV.xlsx",
+  "Obracunato_ha.xlsx",
+  "OpisOpste.xlsx",
+  "OpisSastojine.xlsx",
+  "OpisStanista.xlsx",
+  "OpisVrsteDrveca.xlsx",
+  "osn_dst.xlsx",
+  "Osn_Krive_ZD.xlsx",
+];
 
 //Iterate through subfolders
-//Check if excel file is empty (only one row with column titles)
-//Delete empty files
+//Check if excel file is empty (only one row with column titles) and delete it
 //Check missing columns in files with same name, or any difference in column names or order
 //return JSON.stringify(a1)==JSON.stringify(a2);
 //Create new file in root dir with the same name and content from all the other files with the same name.
@@ -36,7 +48,7 @@ let completedFilesArray = ["GranicneLinije.xlsx"];
 
   Promise.all(promiseArray).then(function () {
     if (isThereDifferentColumns) {
-      console.log("There are files with difference in column names. It needs to be corrected.");
+      console.log("There are files with difference in column names. It needs to be corrected.", differenceCount);
     } else {
       console.log("LENGTH", dataArray.length);
       const workSheet = xlsx.utils.aoa_to_sheet(dataArray);
@@ -60,10 +72,11 @@ async function checkColumnTitles(filePath) {
           if (currentTitle !== JSON.stringify(rows[0])) {
             console.log(filePath, JSON.stringify(rows[0]));
             isThereDifferentColumns = true;
+            differenceCount++;
           }
           dataArray = dataArray.concat(rows.slice(1));
-          console.log(filePath, rows.length);
-          console.log("dataLength", dataArray.length);
+          //console.log(filePath, rows.length);
+          //console.log("dataLength", dataArray.length);
         } else {
           currentTitle = JSON.stringify(rows[0]);
           dataArray = [...rows];
@@ -87,7 +100,6 @@ async function deleteFile(filePath) {
   readXlsxFile(filePath)
     .then((rows) => {
       if (rows.length <= 1) {
-        console.log("empty", filePath);
         fs.unlinkSync(filePath);
       }
     })
@@ -96,11 +108,25 @@ async function deleteFile(filePath) {
     });
 }
 
-/*readXlsxFile(excelFilePath).then((rows) => {
-  rows.forEach((documentRow) => {    
+async function deleteFiles() {
+  const rootDir = fs.opendirSync(rootFolder);
+  let subDir;
+  while ((subDir = rootDir.readSync()) !== null) {
+    let subFolderPath = rootFolder + subDir.name + "/";
+    let currentDir = fs.opendirSync(subFolderPath);
+    let fileName;
+    while ((fileName = currentDir.readSync()) !== null) {
+      //await deleteFile(subFolderPath + fileName.name);
+      completedFilesArray.forEach((el) => {
+        if (fileName.name === el) {
+          console.log("File", fileName.name);
+          fs.unlinkSync(subFolderPath + el);
+        }
+      });
+    }
+    currentDir.closeSync();
+  }
+  rootDir.closeSync();
+}
 
-  });
-
-  // `rows` is an array of rows
-  // each row being an array of cells.
-});*/
+//deleteFiles();
